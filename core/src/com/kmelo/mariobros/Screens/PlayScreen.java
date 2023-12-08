66e0d19bb61e24f20b55d2352cb828e78b0db945
K1Melo <kauafernandes.smelo@gmail.com>
2023-12-08 20:36:10 -0300

    feat: Fit viewport

--- core/src/com/kmelo/mariobros/Screens/PlayScreen.java
+++ core/src/com/kmelo/mariobros/Screens/PlayScreen.java
@@ -3,16 +3,25 @@ package com.kmelo.mariobros.Screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.kmelo.mariobros.MarioBros;
 
 public class PlayScreen implements Screen {
 
     private MarioBros game;
     Texture texture;
+    private OrthographicCamera gameView;
+    private Viewport gamePortable;
+
     public PlayScreen(MarioBros game) {
         this.game = game;
         texture = new Texture("badlogic.jpg");
+        gameView = new OrthographicCamera();
+        gamePortable = new FitViewport(800, 480, gameView);
     }
     @Override
     public void show() {
@@ -23,6 +32,7 @@ public class PlayScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.setProjectionMatrix(gameView.combined);
         game.batch.begin();
         game.batch.draw(texture, 0, 0);
         game.batch.end();
@@ -30,7 +40,7 @@ public class PlayScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-
+        gamePortable.update(width, height);
     }
 
     @Override
