7d0161dd53bf4898a176e1d20467ee632b12810a
K1Melo <kauafernandes.smelo@gmail.com>
2023-12-09 17:45:14 -0300

    feat: Adding mario sprite

--- core/src/com/kmelo/mariobros/Screens/PlayScreen.java
+++ core/src/com/kmelo/mariobros/Screens/PlayScreen.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
@@ -41,8 +42,11 @@ public class PlayScreen implements Screen {
     private Box2DDebugRenderer b2dr;
 
     private Mario player;
+    private TextureAtlas atlas;
 
     public PlayScreen(MarioBros game) {
+        atlas = new TextureAtlas("Mario_and_Enimies.pack");
+
         this.game = game;
 
         gameView = new OrthographicCamera();
@@ -73,8 +77,13 @@ public class PlayScreen implements Screen {
             new Brick(world, map, rect);
         }
 
-        player = new Mario(this.world);
+        player = new Mario(world, this);
     }
+
+    public TextureAtlas getAtlas() {
+        return atlas;
+    }
+
     @Override
     public void show() {
         
@@ -99,6 +108,8 @@ public class PlayScreen implements Screen {
 
         world.step(1/60f, 6, 2);
 
+        player.update(dt);
+
         gameView.position.x = player.b2body.getPosition().x;
 
         gameView.update();
@@ -115,6 +126,11 @@ public class PlayScreen implements Screen {
         renderer.render();
 
         b2dr.render(world, gameView.combined);
+
+        game.batch.setProjectionMatrix(gameView.combined);
+        game.batch.begin();
+        player.draw(game.batch);
+        game.batch.end();
 
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
--- core/src/com/kmelo/mariobros/Sprites/Mario.java
+++ core/src/com/kmelo/mariobros/Sprites/Mario.java
@@ -1,16 +1,27 @@
 package com.kmelo.mariobros.Sprites;
 
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.physics.box2d.*;
 import com.kmelo.mariobros.MarioBros;
+import com.kmelo.mariobros.Screens.PlayScreen;
 
 public class Mario extends Sprite {
     public World world;
     public Body b2body;
+    private TextureRegion marioStand;
 
-    public Mario(World world) {
+    public Mario(World world, PlayScreen screen) {
+        super(screen.getAtlas().findRegion("little_mario"));
         this.world = world;
         defineMario();
+        marioStand = new TextureRegion(getTexture(), 0, 0, 16, 16);
+        setBounds(0, 0, (float) 16 / MarioBros.PPM, (float) 16 / MarioBros.PPM);
+        setRegion(marioStand);
+    }
+
+    public void update(float dt) {
+        setPosition(b2body.getPosition().x - getWidth() / 2, b2body.getPosition().y - getHeight() / 2);
     }
 
     public void  defineMario() {
@@ -21,7 +32,7 @@ public class Mario extends Sprite {
 
         FixtureDef fdef = new FixtureDef();
         CircleShape shape = new CircleShape();
-        shape.setRadius((float) 5 / MarioBros.PPM);
+        shape.setRadius((float) 6 / MarioBros.PPM);
 
         fdef.shape = shape;
         b2body.createFixture(fdef);
