9cc606f4ceb76ac00fc3ebf934d2ecf205317a1b
K1Melo <kauafernandes.smelo@gmail.com>
2023-12-09 13:47:21 -0300

    feat: First level map

--- core/src/com/kmelo/mariobros/Screens/PlayScreen.java
+++ core/src/com/kmelo/mariobros/Screens/PlayScreen.java
@@ -5,6 +5,9 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.kmelo.mariobros.MarioBros;
@@ -18,6 +21,10 @@ public class PlayScreen implements Screen {
     private Viewport gamePortable;
     private Hud hud;
 
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+
     public PlayScreen(MarioBros game) {
         this.game = game;
 
@@ -25,16 +32,38 @@ public class PlayScreen implements Screen {
         gamePortable = new FitViewport(MarioBros.V_WIDTH, MarioBros.V_HEIGHT, gameView);
         hud = new Hud(game.batch);
 
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("level1.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+        gameView.position.set(gamePortable.getWorldWidth() / 2, gamePortable.getWorldHeight() / 2, 0);
     }
     @Override
     public void show() {
         
     }
 
+    public void handleInput(float dt) {
+        if(Gdx.input.isTouched()) {
+            gameView.position.x += 100 * dt;
+        }
+
+    }
+
+    public void update(float dt) {
+        handleInput(dt);
+
+        gameView.update();
+        renderer.setView(gameView);
+    }
+
     @Override
     public void render(float delta) {
+        update(delta);
+
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        renderer.render();
 
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
